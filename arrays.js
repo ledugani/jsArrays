0ece89baf739645faf015f6a5a57fe2f0958fd13
Tom Dugan <thomas@aadwebb.com>
2018-02-27 19:46:46 -0600

    completed first challenge

--- arrays.js
+++ arrays.js
@@ -1,1 +1,11 @@
 console.log();
+
+// Challenge #1
+var test1 = "mom";
+var palindrome = test1.split("").reverse().join("");
+
+if (test1 === palindrome) {
+    console.log("true");
+} else {
+    console.log("false");
+}
