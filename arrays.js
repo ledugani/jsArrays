47cc54495d173a695acb59011e4882f11e155780
Tom Dugan <thomas@aadwebb.com>
2018-02-27 21:18:35 -0600

    completed challenge 2

--- arrays.js
+++ arrays.js
@@ -9,3 +9,34 @@ if (test1 === palindrome) {
 } else {
     console.log("false");
 }
+
+// Example A
+var animals = ['fish', 'dog', 'cat'];
+var animalzElement = document.getElementById('animalz');
+for (var i=0; i<animals.length; i++) {
+    // console.log(animals[i])
+    animalzElement.innerHTML += '<h5>' + animals[i] + '</h5>';
+}
+
+// Challenge #2
+var book1 = "Catch 22";
+var book2 = "Slaughterhouse 5";
+var book3 = "Fahrenheit 451";
+var myBooks = [book1, book2, book3];
+
+for (var j=0; j<myBooks.length; j++) {
+    console.log(
+        myBooks[j].split(" ").pop()
+    );
+}
+
+// /*- Zoe's way -*/
+// var book = "catch 22";
+// var bookSplit = book.split("");
+// var realNumz = [];
+// for(var k=0; k<bookSplit.length; k++) {
+//     if (bookSplit[k]*1) {
+//         realNumz.push(bookSplit[k]);
+//     }
+// }
+// console.log("answer", realNumz.join(""))
